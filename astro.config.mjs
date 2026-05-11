107d0673ebe7cbf1de3b00c3dc58205997f2d53b
Fabien Grignoux <fabien.grignoux@outlook.com>
2026-05-11 14:59:27 +0200

    Gtag + partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,33 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@astrojs/vue';
-
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://latelierpsy.com',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [vue(), mdx()],
+  output: 'static',
+
+  adapter: netlify(),
+
+  integrations: [
+    vue(),
+    mdx(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+    sitemap(),
+  ],
 
   image: {
     remotePatterns: [
